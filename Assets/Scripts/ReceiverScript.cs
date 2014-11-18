d432efb09d77f069718bae8d4d35e5f4f747283c
Emre Tanirgan <tanirganemre@gmail.com>
2014-11-18 17:24:24 -0500

    Added receiver script and receiver virtual object in level 1

---
+++ Assets/Scripts/ReceiverScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceiverScript : MonoBehaviour {
+
+	public bool received = false;
+	public float winTimer = 3.0f; //how many seconds the light needs to hit until the player wins
+	public bool win = false; //becomes true after the timer becomes 0 and the ray is still hitting
+	public float expectedIntensity = 0.2f; //will be used for levels with dividers
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(received){
+			winTimer -= Time.deltaTime;
+		}
+		if (winTimer <= 0){
+			win = true;
+		}
+	}
+
+	void OnRayEnter(RayInfo ray) {
+		received = true;
+		//Get the intensity of the incoming ray
+		float receivedIntensity = ray.intensity;
+	}
+
+	void onRayExit() {
+		//reset the timer on ray exit
+		received = false;
+		winTimer = 3.0f;
+	}
+}
