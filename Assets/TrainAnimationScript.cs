ff53b5c687d99f253c093702e7b47e9e9fc09f34
foxking0416 <foxking0416@gmail.com>
2014-11-18 17:09:05 -0500

    add animation

---
+++ Assets/TrainAnimationScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainAnimationScript : MonoBehaviour {
+	public Transform[] path;
+	float t = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion q = new Quaternion();
+		transform.position	 = Spline.MoveOnPath(path, transform.position, ref t, ref q, 10.0f, 100, EasingType.Sine, true, true);
+		//transform.localPosition	 = Spline.MoveOnPath(path, transform.localPosition, ref t, ref q, 10.0f, 100, EasingType.Sine, true, true);
+		//transform.position = Spline.MoveOnPath(path, transform.position, ref t, 10.5f);
+		//transform.rotation = q;
+
+	}
+}
