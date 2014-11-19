92245def15c0dce98cbff746ad1bb0663b521b9c
foxking0416 <foxking0416@gmail.com>
2014-11-18 22:01:53 -0500

    add restart button

--- Assets/GlobalScript.cs
+++ Assets/GlobalScript.cs
@@ -3,28 +3,19 @@ using System.Collections;
 
 public class GlobalScript : MonoBehaviour {
 	
-	//GameObject fighter;
-	//FighterScript fighterScript;
 
 	void Start () {
-		//fighter = GameObject.FindGameObjectWithTag("Fighter");
-		//fighterScript = fighter.GetComponent<FighterScript> ();
+
 	}
 
 	void OnGUI (){
 		GUILayout.BeginArea(new Rect(0,  100, 100, Screen.height));//Screen.height / 2 +
 
-		if (GUILayout.Button("Fire", GUILayout.Height(300))) {
-			//Application.LoadLevel("GameplayScene");
-			//fighterScript.Fire();
-			//launcher.LaunchRay();
+		if (GUILayout.Button("Restart", GUILayout.Height(300))) {
+			Application.LoadLevel("Level1Scene");
 		}
 
-		
-		/*if (GUILayout.Button("Exit")) {
-			Application.Quit();
-			Debug.Log ("Application.Quit() only works in build, not in editor");
-		}*/
+
 		GUILayout.EndArea();
 	}
 
